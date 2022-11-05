d648b75e42818d5b1db47e4b19c3e32b15808f97
Hironari Kitamura <hiro@Hironaris-MacBook-Air.local>
2022-11-05 19:08:25 +0900

    first setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import type { NextPage } from "next";
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -13,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js !!!</a>
         </h1>
 
         <p className={styles.description}>
